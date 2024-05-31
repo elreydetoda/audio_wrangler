7076a9aa14c4f714d57c29f82e7053ab79553e3d
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2024-05-31 08:37:12 -0400

    initial test validation

---
+++ tests/conftest.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+import hashlib
+from pytest import fixture
+from src.whisper_interface import WhisperInterface
+
+
+@fixture(scope="session")
+def example_file() -> Path:
+    """Return a Path object to a sample audio file."""
+    return Path("tests/assets/240530_1653.wav")
+
+
+@fixture(scope="session")
+def transcribed_file() -> str:
+    """Return the expected transcribed text from the example file."""
+    return "I think it's in part, there's just config drift and things in there that kind of come along. And there's lots of little updates here that you don't really have any control over. So I propose to build it better."
+
+
+@fixture(scope="session")
+def whisper_interface():
+    """Return a WhisperInterface object."""
+    return WhisperInterface()
+
+
+@fixture(scope="session")
+def whisper_transcribed(whisper_interface, example_file):
+    """Return the transcribed text from the example file using the WhisperInterface object."""
+    return whisper_interface.transcribe(example_file).get("text").strip()
+
+
+@staticmethod
+def get_md5sum(input_string):
+    """Return the md5 checksum for the input string."""
+    # Step 1: Create an md5 hash object
+    md5_hash = hashlib.md5()
+
+    # Step 2: Encode the string to bytes
+    encoded_string = input_string.encode("utf-8")
+
+    # Step 3: Update the hash object with the encoded string
+    md5_hash.update(encoded_string)
+
+    # Step 4: Get the hexadecimal representation of the checksum
+    md5_checksum = md5_hash.hexdigest()
+
+    return md5_checksum
---
+++ tests/test_main.py
@@ -0,0 +1,7 @@
+# from src.main import get_file
+
+
+# def test_get_file(example_file) -> bytes:
+#     file_contents = get_file(example_file)
+#     assert file_contents == example_file.read_bytes()
+#     assert isinstance(file_contents, bytes)

a74672b1d48b20c022f9269ba8e48485385bf354
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2024-06-02 11:34:52 -0400

    finished tutorial

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 import re
+from time import monotonic
 
 from sqlmodel import Session
 from whisper.utils import get_writer
@@ -9,6 +10,7 @@ from textual import on
 from textual.app import App
 from textual.widgets import Header, Footer, Button, Static
 from textual.containers import ScrollableContainer
+from textual.reactive import reactive
 
 from models.db_models import FilesMetadata
 from backend.whisper_interface import WhisperInterface
@@ -37,26 +39,72 @@ def process_transcription(wsp: WhisperInterface, input_file: Path) -> Path:
     return modify_text_output(input_file.with_suffix(".txt"))
 
 
+class TimeDisplay(Static):
+
+    accumulated_time = 0
+    time_elapsed = reactive(0)
+
+    # handler method
+    def on_mount(self):
+        """handles when class is loaded/rendered"""
+        self.update_timer = self.set_interval(
+            1 / 60, self.update_time_elapsed, pause=True  # 60 fps
+        )
+
+    def update_time_elapsed(self):
+        self.time_elapsed = self.accumulated_time + (monotonic() - self.start_time)
+
+    def watch_time_elapsed(self):
+        time = self.time_elapsed
+        minutes, seconds = divmod(time, 60)
+        hours, minutes = divmod(minutes, 60)
+        self.update(f"{hours:02,.0f}:{minutes:02.0f}:{seconds:05.2f}")
+        # self.update(str(time))
+
+    def start(self):
+        """"""
+        self.start_time = monotonic()
+        self.update_timer.resume()
+
+    def stop(self):
+        """"""
+        self.accumulated_time = self.time_elapsed
+        self.update_timer.pause()
+
+    def reset(self):
+        """"""
+        self.accumulated_time = 0
+        self.time_elapsed = 0
+
+
 class AudioWrangler(Static):
     # handling messages in textual
     @on(Button.Pressed, "#first")
     def first_pressed_method(self):
-        self.app.exit()
+        # self.app.exit()
+        self.query_one(TimeDisplay).start()
 
     @on(Button.Pressed, "#second")
     def second_pressed_method(self):
-        self.add_class("hidden")
+        # self.add_class("hidden")
+        # time_display = self.query_one(TimeDisplay)
+        # time_display.time_elapsed = 7.3
+        self.query_one(TimeDisplay).stop()
 
     def compose(self):
         yield Button("First", id="first")
         yield Button("Second", id="second")
+        yield TimeDisplay("00:00:00.00")
         # yield Button("Third", variant="error", id="stop", classes="hidden")
 
 
 class AudioWranglerApp(App):
     BINDINGS = [
         # ("keybinding", "function_name", "description")
-        # ("d","toggle_dark_mode", "Toggle dark mode")
+        ("d", "toggle_dark_mode", "Toggle dark mode"),
+        ("a", "add_audiowrangler", "Add"),
+        ("r", "remove_audiowrangler", "Add"),
+        # actual function needs to have a prefix of action_<above_name>
     ]
 
     CSS_PATH = "frontend/style.tcss"
@@ -67,6 +115,20 @@ class AudioWranglerApp(App):
         with ScrollableContainer(id="wranglers"):
             yield AudioWrangler()
             yield AudioWrangler()
+
+    def action_toggle_dark_mode(self):
+        self.dark = not self.dark
+
+    def action_add_audiowrangler(self):
+        audiowrangler = AudioWrangler()
+        conttainer = self.query_one("#wranglers")
+        conttainer.mount(audiowrangler)
+        audiowrangler.scroll_visible()
+
+    def action_remove_audiowrangler(self):
+        wranglers = self.query(AudioWrangler)
+        if wranglers:
+            wranglers.last().remove()
 
 
 def main():
